59e96f183c87e6025b648a67b2daf35314194c5e
Azamat <azamatomonkeldiyev@gmail.com>
2025-06-18 01:45:33 +0500

    added tickets page

--- src/modules/Headers.jsx
+++ src/modules/Headers.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { navList } from '../hooks/paths';
+import { NotificationIcon, SearchIcon } from '../assets/icons';
 
 const Headers = () => {
   const {pathname} = useLocation();
+
+  const [ showInput, setShowInput ] = useState(false)
+  function handleBlur(e) {
+    setTimeout(() => {
+      setShowInput(false)
+      e.target.value = null
+    }, 2000)
+  }
+
   return (
     <div className='flex items-center justify-between mb-[58px]'>
       <strong className='font-bold text-[24px] text-[#252733]'>{navList.find(item => item.path == pathname).title}</strong>
-      <div></div>
+       <div className='flex items-center gap-[32px]'>
+        <div className='flex items-center gap-6'>
+          <div className='flex items-center'>
+            <button className={`${showInput ? 'hidden' : ''} cursor-pointer`} onClick={() => setShowInput(true)}><SearchIcon /></button>
+            <input onBlur={handleBlur} className={`border-[1px] duration-400 rounded-2xl outline-none ${showInput ? 'focus:shadow-sm shadow-blue-400 py-1 pl-2 border-slate-200 w-[190px]' : 'w-0 border-transparent'}`} placeholder='Searching...' type="text" />
+          </div>
+          <button><NotificationIcon /></button>
+        </div>
+        <span className='w-[1px] h-[32px] inline-block bg-[#DFE0EB]'></span>
+        <div className='flex items-center gap-6'>
+          <p className='text-[#252733] font-semibold text-[14px] '>Jones Ferdinand</p>
+          <img className='w-[44px] h-[44px]' src='src/assets/images/avatar.svg' alt="Avatar Icon" width={44} height={44} />
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import TicketsTableTopAction from '../components/TicketTableTopAction'
+import TicketTable from '../modules/TicketTable'
 
 const Tickets = () => {
+  const navigate = useNavigate()
   return (
-    <div>Tickets</div>
+    <div className='bg-white py-[32px] rounded-[8px] border-[1px] border-[#DFE0EB]'>
+      <div className='pb-[48px] px-[32px] flex justify-between items-center'>
+        <h2 className='font-bold text-[19px] text-[#252733]'>All tickets</h2>
+        <div className='flex items-center gap-[32px]'>
+          <TicketsTableTopAction title={'Sort'} type={'sort'}/>
+          <TicketsTableTopAction title={'Filter'}/>
+          <button onClick={() => navigate('create')} className='text-white font-bold text-[14px] w-[90px] rounded-[20px] p-2 bg-[#252733] cursor-pointer'>+</button>
+        </div>
+      </div>
+      <TicketTable />
+    </div>
   )
 }
 
